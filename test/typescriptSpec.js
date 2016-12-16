52cd9fe8b4b07c0fa0cd36d3118cdd83f369112f
Calvin Wiebe <cwiebe@xtreme-eda.com>
2016-12-16 17:11:50 -0500

    Cleanup test file

--- test/typescriptSpec.js
+++ test/typescriptSpec.js
@@ -1,14 +1,14 @@
 'use strict';
 
 let {exec} = require('child_process');
-let path = require('path');
 let {expect} = require('chai');
 
+// This just makes sure the `typescript.ts` file compiles
 describe('Typescript', () => {
     describe('build', () => {
-        it('must build without error', function (done) {
+        it('must build without error', (done) => {
             this.timeout(10000);
-            exec('tsc --noEmit', {cwd: 'test', }, function (error) {
+            exec('tsc --noEmit', {cwd: 'test'}, (error) => {
                 expect(error).to.eq(null);
                 done();
             });
